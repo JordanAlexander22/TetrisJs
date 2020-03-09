3c85f9d1e9769728fd97baaf3b58dacddb952618
jordan <jordan@jordans-MacBook-Air.local>
2020-03-09 18:52:46 +0700

    started working on functionality

---
+++ tetris.js
@@ -0,0 +1,12 @@
+
+
+
+const cvs = document.getElementById("Tetris");
+const ctx = cvs.getContext("2d");
+
+const ROW = 20;
+const COL = COLUMN = 10;
+const SQ = squareSize = 20;
+const VACANT = "WHITE"; //color of the empty square
+
+// draw a square on the canvas 
