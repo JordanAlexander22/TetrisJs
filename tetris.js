99ec02986d0d34dc27ee91efd9e81688a8d970a8
jordan <jordan@jordans-MacBook-Air.local>
2020-03-13 20:24:57 +0700

    have board drawn out

--- tetris.js
+++ tetris.js
@@ -16,12 +16,44 @@ const VACANT = "WHITE"; //color of the empty square
 
 // create a function to make above code reuseable ^
 
-function drawSquare(x,y,color) {
+function drawSquare(x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x*SQ, y*SQ, 50, 50);
+  ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
 
   ctx.strokeStyle = "BLACK";
-  ctx.strokeRect(x*SQ, y*SQ, 50, 50);
+  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+};
+
+// time to create the board
+
+let board = [];
+for (r = 0; r < ROW; r++) {
+  board[r] = [];
+  for (c = 0; c < COL; c++) {
+    board[r][c] = VACANT;
+  }
 }
 
-drawSquare(0,0,"purple");
+//need to draw board to the canvas
+function drawBoard() {
+  for (r = 0; r < ROW; r++) {
+    for (c = 0; c < COL; c++) {
+      drawSquare(c,r,board[r][c]);
+    }
+  }
+};
+
+drawBoard();
+
+//object piece 
+
+function Piece(tetromino, color){
+    this.tetromino= tetromino;
+    this.color= color;
+
+    this.tetrominoNum= 0;  // starting from first pattern
+    this.activeTetromino= this.tetromino[this.tetrominoNum];
+
+    // must implement control of pieces 
+}
+
