c5a19fc4db7bf4f82feeea79310d36719d050a1e
jordan <jordan@jordans-MacBook-Air.local>
2020-03-13 19:54:58 +0700

    created function to begin drawing shapes

--- tetris.js
+++ tetris.js
@@ -1,12 +1,27 @@
-
-
-
 const cvs = document.getElementById("Tetris");
 const ctx = cvs.getContext("2d");
 
 const ROW = 20;
-const COL = COLUMN = 10;
-const SQ = squareSize = 20;
+const COL = (COLUMN = 10);
+const SQ = (squareSize = 20);
 const VACANT = "WHITE"; //color of the empty square
 
-// draw a square on the canvas 
+// draw a square on the canvas
+
+// ctx.fillStyle = "red";
+// ctx.fillRect(0, 0, 50, 50);
+
+// ctx.strokeStyle = "BLACK";
+// ctx.strokeRect(0, 0, 50, 50);
+
+// create a function to make above code reuseable ^
+
+function drawSquare(x,y,color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x*SQ, y*SQ, 50, 50);
+
+  ctx.strokeStyle = "BLACK";
+  ctx.strokeRect(x*SQ, y*SQ, 50, 50);
+}
+
+drawSquare(0,0,"purple");
