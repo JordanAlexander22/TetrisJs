20f499f5f1c3eb54a3edb0381cdd7de7e9fbca71
jordan <jordan@jordans-MacBook-Air.local>
2020-04-29 22:01:34 -0700

    piece is able to move

--- tetris.js
+++ tetris.js
@@ -107,8 +107,51 @@ Piece.prototype.moveDown = function () {
     this.draw();  
 }
 
-// piece gets dropped every second 
 
+// piece is able to move right
+Piece.prototype.moveRight = function () {
+  this.unDraw();
+  this.x++;
+  this.draw();  
+}
+
+
+// piece is able to move left
+Piece.prototype.moveLeft = function () {
+  this.unDraw();
+  this.x--;
+  this.draw();  
+}
+
+
+// rotating the piece
+Piece.prototype.rotate = function () {
+  this.unDraw();
+  this.tetrominoNum= (this.tetrominoNum+1)%this.tetromino.length; // (0+1)%4= 4
+  this.activeTetromino= this.tetromino[this.tetrominoNum]
+  this.draw();  
+}
+
+
+// Controlling the piece
+
+document.addEventListener('keydown', CONTROL );
+
+function CONTROL (event) {
+  if (event.keyCode == 37) {
+    p.moveLeft();
+  } else if (event.keyCode == 38){
+    p.rotate();
+  } else if (event.keyCode == 39){
+    p.moveRight();
+  } else if (event.keyCode == 40){
+    p.moveDown();
+  }
+}
+
+
+
+// piece gets dropped every second 
 let dropStart = Date.now();
 function drop () {
     let now = Date.now();
